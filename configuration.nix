863556290f76aa49ffc4caa71945955ff937f88a
voltamage <37029882+voltamage@users.noreply.github.com>
2024-01-06 16:44:51 -0800

    add packages to configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -65,6 +65,10 @@
   environment.systemPackages = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
+  git
+  lazygit
+  neovim
+  ranger
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
