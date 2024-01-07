152f41f023f172d7dc1236c9ccb1d603388ebc64
voltamage <37029882+voltamage@users.noreply.github.com>
2024-01-06 16:42:34 -0800

    add initial flake

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+
+  description = "Initial flake";
+
+  inputs = {
+    #nixpkgs = {
+    #  url = "github:NixOS/nixpkgs/nixos-23.11";
+    #};
+    nixpkgs.url = "nixpkgs/nixos-23.11";
+  };
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      lib = nixpkgs.lib;
+    in {
+    nixosConfigurations = {
+      nixos = lib.nixosSystem {
+        system = "x86_64-linux";
+	modules = [ ./configuration.nix ];
+      };
+    };
+  };
+}
